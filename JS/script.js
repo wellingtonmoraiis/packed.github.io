35c2158a433638a0d1b21d4f63a5ce74e223af0b
Wellington de Morais <wellington.moraiis@gmail.com>
2023-04-16 16:33:37 -0300

    pequenas correções

--- JS/script.js
+++ JS/script.js
@@ -10,4 +10,17 @@ function toggleMenu(event){
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
+  //-----------------------
 
+
+function Enviar(){
+  const nome = document.querySelector('#nome');
+
+  const email = document.querySelector('#email');
+  if(email.value === ''){
+    alert('Email é obrigatório')
+    return;
+  }
+  alert('enviado!');
+
+}
